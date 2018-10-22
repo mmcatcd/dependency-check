f64c1fb7d772893b9a53e0df81b01b6cf30340d7
Michael McAndrew <mcandrmi@tcd.ie>
2018-10-22 23:50:03 +0100

    Updated Example and added sample example data.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Logo from './components/Logo';
+import SearchBar from './components/SearchBar';
+import Colors from './components/Colors';
+import Example from './components/Example';
+
+const tags = ['javascript', 'nodejs', 'express', 'server'];
+
+const exampleData = [
+  {
+    title: "Express",
+    description: "Fast, unopinionated, minimalist web framework for node.",
+    tags: ['javascript', 'nodejs', 'express', 'server'],
+    commit: {
+      author: "dougwilson",
+      date: "2018-10-11T03:50:45Z",
+      description: "4.16.4"
+    }
+  },
+  {
+    title: "SwiftyJSON",
+    description: "The better way to deal with JSON data in Swift.",
+    tags: ['swiftyjson', 'json', 'swift', 'xcode10', 'swift4-2', 
+      'response', 'request', 'carthage', 'cocoapods', 'json-parsing-library',
+      'json-parser', 'json-parsing-swift'],
+    commit: {
+      author: "wrongzigii",
+      date: "2018-09-26T10:03:28Z",
+      description: "Update README.md"
+    }
+  }
+]
+
+const examples = exampleData.map(repo => {
+  return (
+    <Example 
+      tags={tags} 
+      title={repo.title}
+      description={repo.description}
+      commitAuthor={repo.commit.author}
+      commitDate={repo.commit.date}
+      commitDescription={repo.commit.description}
+       />);
+  });
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div style={styles.searchContainer} className="searchContainer">
+          <div style={styles.logoContainer}><Logo /></div>
+          <div style={styles.searchBarContainer}><SearchBar /></div>
+          <div style={styles.exampleContainer}>
+            <h2 style={styles.exampleHeader}>Examples</h2>
+            {examples}
+          </div>
+        </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  searchContainer: {
+    display: 'block',
+    maxWidth: 800,
+    margin: 'auto',
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  logoContainer: {
+    marginTop: 30,
+    marginBottom: 40
+  },
+  searchBarContainer: {
+    padding: '0 10px'
+  },
+  exampleContainer: {
+    marginTop: 20,
+    overflow: 'auto',
+    padding: '0 10px'
+  },
+  exampleHeader: {
+    color: Colors.purple
   }
 }
 
