27a0908f97b882675dbd0136c7acd7d62210f77a
Michael McAndrew <mcandrmi@tcd.ie>
2018-11-06 15:22:21 +0000

    Added morgan for logging and standard json response for root path.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { ApolloServer } = require('apollo-server-express');
+const morgan = require('morgan');
 
 const typeDefs = require('./graphql/schema'); // Import GraphQL schema
 const resolvers = require('./graphql/resolvers'); // Import GraphQL query resolvers
@@ -8,6 +9,18 @@ const resolvers = require('./graphql/resolvers'); // Import GraphQL query resolv
 // Create a new Apollo Server with Express middleware.
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 apolloServer.applyMiddleware({ app });
+
+// Console logging
+app.use(morgan('dev'));
+
+// Successful request response
+app.use(express.Router().get('/', (req, res, next) => {
+  res.status(200).json({
+    message: "You've found Dependency Check web server.",
+    IP: req.connection.remoteAddress,
+    graphQLPath: apolloServer.graphqlPath
+  });
+}));
 
 module.exports = app;
 module.exports.apolloServer = apolloServer;
