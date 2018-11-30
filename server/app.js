6869a88a638e14ae42f80f932fed302edb4810c4
Michael McAndrew <mcandrmi@tcd.ie>
2018-11-30 14:32:24 +0000

    Added CORS compatibility for endpoints

--- server/app.js
+++ server/app.js
@@ -8,6 +8,20 @@ const morgan = require('morgan');
 // Console logging
 app.use(morgan('dev'));
 
+//CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes
 app.use('/api', routes);
 
